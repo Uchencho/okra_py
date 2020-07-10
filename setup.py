7c535a9f69cabd43053bf0df802380c77d1cc7ca
Uchencho <aloziekelechi17@gmail.com>
2020-07-10 15:29:08 +0100

    production ready changes

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+LICENSE = 'Apache License 2.0'
+
+setup(name='okra_py',
+    version='1.0',
+    description='Python wrapper for Okra API',
+    url='https://github.com/Uchencho/okra_py',
+    author="Uchenna Alozie",
+    author_email='alozieuche17@gmail.com,
+    license=LICENSE,
+    install_requires=['requests'],
+    packages=['okra_py'],
+    )
