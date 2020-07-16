a49fddde4d21b8edbd6a0027d13c7d67bde9e6ee
Uchencho <aloziekelechi17@gmail.com>
2020-07-16 20:20:17 +0100

    environment variable issues

--- tests/okra_test.py
+++ tests/okra_test.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import os
 # from settings import token
 
 from okra_py.auth import Okra_Auth
@@ -8,15 +9,17 @@ from okra_py.transaction import OkraTransaction
 from okra_py.identity import OkraIdentify
 from okra_py.income import OkraIncome
 
+token = os.environ.get('token')
+
 class OkraAuthTest(TestCase):
     def test_a_token_error(self):
         with self.assertRaises(MissingTokenError) as context:
             Okra_Auth()
         self.assertTrue(context.exception)
 
-    # def test_b_valid_retreive_resp(self):
-    #     okra_mod = Okra_Auth(token)
-    #     self.assertEqual(okra_mod.retrieve_auth().status_code, 200)
+    def test_b_valid_retreive_resp(self):
+        okra_mod = Okra_Auth(token)
+        self.assertEqual(okra_mod.retrieve_auth().status_code, 200)
 
 
 class OkraBalanceTest(TestCase):
@@ -25,9 +28,9 @@ class OkraBalanceTest(TestCase):
             Okra_Balance()
         self.assertTrue(context.exception)
 
-    # def test_b_valid_retreive_balance_response(self):
-    #     okra_mod = Okra_Balance(token)
-    #     self.assertEqual(okra_mod.retrieve_balance().status_code, 200)
+    def test_b_valid_retreive_balance_response(self):
+        okra_mod = Okra_Balance(token)
+        self.assertEqual(okra_mod.retrieve_balance().status_code, 200)
 
 
 class OkraTransactionTest(TestCase):
@@ -36,9 +39,9 @@ class OkraTransactionTest(TestCase):
             OkraTransaction()
         self.assertTrue(context.exception)
 
-    # def test_b_valid_retreive_transaction_response(self):
-    #     okra_mod = OkraTransaction(token)
-    #     self.assertEqual(okra_mod.getTransactions().status_code, 200)
+    def test_b_valid_retreive_transaction_response(self):
+        okra_mod = OkraTransaction(token)
+        self.assertEqual(okra_mod.getTransactions().status_code, 200)
 
 
 class OkraIdentityTest(TestCase):
@@ -54,6 +57,11 @@ class OkraIncomeTest(TestCase):
             OkraIncome()
         self.assertTrue(context.exception)
 
-    # def test_b_valid_retreive_income_response(self):
-    #     okra_mod = OkraIncome(token)
-    #     self.assertEqual(okra_mod.getIncome().status_code, 200)
+    def test_b_valid_retreive_income_response(self):
+        okra_mod = OkraIncome(token)
+        self.assertEqual(okra_mod.getIncome().status_code, 200)
+
+
+
+# Set environment variables
+# Update Readme
